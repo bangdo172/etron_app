5a7f96cc6783ec32f475106c3d1351c77389e6d7
tuyenhuy <tuyenhuy026@gmail.com>
2020-09-20 17:32:56 +0700

    add table template

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    context = {'latest_question_list': [1, 2, 3]}
+    return render(request, 'notes/index.html', context)
+
+    # return HttpResponse("Hello, world. You're at the polls index.")
