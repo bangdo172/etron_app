3dbb8956da2ea9ac5d29993396d4759514db5e2e
tuyenhuy <tuyenhuy026@gmail.com>
2020-10-11 20:13:58 +0700

    Add wit

--- notes/nlp/utils.py
+++ notes/nlp/utils.py
@@ -1,7 +1,12 @@
+import re
+from ..models import Task
+
 def update(text, usre_id, requirement_type):
     pass 
 
-def remind(text, usre_id, requirement_type):
+def remind(text, user_id):
+    text = re.sub("^nhắc tôi ", "", text)
+    
     pass
 
 def query(text, usre_id, requirement_type):
--- notes/nlp/witai.py
+++ notes/nlp/witai.py
@@ -1,29 +1,10 @@
-from wit import Wit
+from .wit import Wit
 
-token = "RRVGH4Z5DP57IDPJ2NXQPEBHJH6UWRUT"
-text = "hẹn họp với Bằng chiều chủ nhật tuần này"
-
-client = Wit(access_token=token)
-wit_resp = client.message(text)
-
-query_text = wit_resp["text"]
-query_intent = wit_resp["intents"][0]["name"]
-query_entities = wit_resp["entities"]
-
-class NLP:
+class WitEndpoint:
     def __init__(self):
         token = "RRVGH4Z5DP57IDPJ2NXQPEBHJH6UWRUT"
         self.wit_client = Wit(access_token=token)
-    
-    def remind(self):
-        pass 
 
-    def query(self):
-        pass
-
-    def update(self):
-        pass 
-
-    def processing(self, request):
-        # request_processing 
+    def processing(self, text):
+        return self.wit_client.message(text)
         pass
