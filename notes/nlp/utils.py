4f03b8dbc5c4c8856440f05579cf129deeb86b48
etron team <etron@etron-demo.us-east1-b.c.flowing-code-281623.internal>
2020-10-11 13:14:34 +0000

    the nao cung confict

--- notes/nlp/utils.py
+++ notes/nlp/utils.py
@@ -1,5 +1,9 @@
-def update(text, usre_id, requirement_type):
-    pass 
+from .keywords import *
+from notes.models import Message, Task, User
+
+def update(text, user_id):
+    pass
+    
 
 def remind(text, usre_id, requirement_type):
     pass
@@ -8,4 +12,13 @@ def query(text, usre_id, requirement_type):
     pass
 
 def classify(text, user_id):
-    pass
+    text = text.lower()
+    for remind_word in REMIND_WORDS:
+        if text.find(remind_word) != -1:
+            return 'remind'
+    
+    for update_word in UPDATE_WORDS:
+        if text.find(update) != -1:
+            return 'update'
+    
+    return 'query'
