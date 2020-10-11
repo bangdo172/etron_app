d185ce4e62e3ab6de43a474ee969abbfaaf13876
tuyenhuy <tuyenhuy026@gmail.com>
2020-10-11 20:20:37 +0700

    fix confict

--- notes/nlp/utils.py
+++ notes/nlp/utils.py
@@ -1,18 +1,25 @@
 import re
-from ..models import Task
 from .keywords import *
-from notes.models import Message, Task, User
+from ..models import Message, Task, User
 
 def update(text, user_id):
     pass
 
 def remind(text, user_id):
     text = re.sub("^nhắc tôi ", "", text)
-
     pass
 
 def query(text, usre_id, requirement_type):
     pass
 
 def classify(text, user_id):
-    pass
+    text = text.lower()
+    for remind_word in REMIND_WORDS:
+        if text.find(remind_word) != -1:
+            return 'remind'
+    
+    for update_word in UPDATE_WORDS:
+        if text.find(update) != -1:
+            return 'update'
+    
+    return 'query'
