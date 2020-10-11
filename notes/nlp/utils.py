25eb41edd54214db7e0fc701b8df80d37e7ab186
tuyenhuy <tuyenhuy026@gmail.com>
2020-10-11 20:19:20 +0700

    fix conflict

--- notes/nlp/utils.py
+++ notes/nlp/utils.py
@@ -1,11 +1,12 @@
+import re
 from .keywords import *
-from notes.models import Message, Task, User
+from ..models import Message, Task, User
 
 def update(text, user_id):
     pass
-    
 
-def remind(text, usre_id, requirement_type):
+def remind(text, user_id):
+    text = re.sub("^nhắc tôi ", "", text)
     pass
 
 def query(text, usre_id, requirement_type):
