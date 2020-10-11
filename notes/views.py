91551459123cd9a780d072a7db7a431696a9c3e7
tuyenhuy <tuyenhuy026@gmail.com>
2020-10-11 20:55:46 +0700

    Update Fontend

--- notes/views.py
+++ notes/views.py
@@ -6,7 +6,7 @@ from rest_framework import response
 from rest_framework import generics
 
 from .serializers import UserSerializer, GroupSerializer
-import nlp
+from notes import nlp
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -44,16 +44,58 @@ class Index(generics.RetrieveAPIView):
             text = "What are you looking for?"
             return response.Response({"text":text}, template_name='notes/index.html')
         
-        requirement_type = nlp.classify(text, user_id)
-        if requirement_type == "remind":
-            context = nlp.remind(text, user_id, requirement_type)
+        # requirement_type = nlp.classify(text, user_id)
+        # if requirement_type == "remind":
+        #     context = nlp.remind(text, user_id, requirement_type)
 
-        if requirement_type == "update":
-            context = nlp.update(text, user_id, requirement_type)
+        # if requirement_type == "update":
+        #     context = nlp.update(text, user_id, requirement_type)
 
-        if requirement_type == "query":
-            context = nlp.query(text, user_id, requirement_type)
+        # if requirement_type == "query":
+        #     context = nlp.query(text, user_id, requirement_type)
         
+        context = {
+            'type': "Remind",
+            'old_task_list': [
+                {
+                    "text": "Like a butterfly",
+                    "people": "Cinda",
+                    "organization": "Baidu",
+                    "location": "Hong Kong",
+                    "start": "2:00 AM",
+                    "expire": "11:00 AM",
+                    "priority": "High",
+                    "status": "Doing",
+                    "version": "1"
+                }
+            ],
+            'task_list': [
+                {
+                    "text": "Like a butterfly",
+                    "people": "Aura",
+                    "organization": "Apple Inc",
+                    "location": "NY City",
+                    "start": "9:00 AM",
+                    "expire": "11:00 AM",
+                    "priority": "High",
+                    "status": "Doing",
+                    "version": "2"
+                },
+                {
+                    "text": "Mind & Body",
+                    "people": "John",
+                    "organization": "Google Inc",
+                    "location": "Paris",
+                    "start": "2:00 AM",
+                    "expire": "5:00 AM",
+                    "priority": "Low",
+                    "status": "Pending",
+                    "version": "1"
+                }
+            ],
+            "text": text
+        }
+
         return response.Response(context, template_name='notes/index.html')
 
 # context = {
